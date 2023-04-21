b4c4adcdc464bbcf04e544e66e4f723373abfd48
lospibes2 <thomas.babioet36@gmail.com>
2023-04-21 09:48:40 -0300

    Sector-vendedor

---
+++ Vendedor.java
@@ -0,0 +1,20 @@
+package com.mycompany.vendedor;
+
+public class Vendedor {
+
+   private double cantVendida;
+    
+    public Vendedor(double cantVendida){
+        super(nombre, CUIT, genero, direccion, telefono, cantHijos, sueldo);
+        this.cantVendida = cantVendida;
+    }
+    public double getcantVendida(){
+        return cantVendida;
+    }
+    public void setcantVendida(){
+        this.cantVendida = cantVendida;
+ 
+        
+    }
+
+}
